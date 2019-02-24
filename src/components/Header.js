dc355e09128ed44a88441f6fe8fad007aa3a09b0
Trevor <trevorme91@gmail.com>
2019-02-24 16:17:58 +0900

    Use inline style for highly dynamic style
    - https://github.com/styled-components/styled-components/issues/134#issuecomment-312415291

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import Appbar from '@material-ui/core/AppBar';
+import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
 import styled from 'styled-components';
@@ -12,15 +12,7 @@ const Wrapper = styled.div({
   display: 'flex',
   justifyContent: 'space-between',
 });
-const StyledAppbar = styled(({ offsetY, ...rest }) => <Appbar {...rest} />)(({ offsetY, theme }) => ({
-  backgroundColor: `rgba(255, 255, 255, ${offsetY/100})`,
-  boxShadow: `0px 2px 4px -1px rgba(0,0,0,${Math.min(
-    0.2,
-    offsetY / THRESHOLD,
-  )}), 0px 4px 5px 0px rgba(0,0,0,${Math.min(
-    0.14,
-    offsetY / THRESHOLD,
-  )}), 0px 1px 10px 0px rgba(0,0,0,${Math.min(0.14, offsetY / THRESHOLD)})`,
+const StyledAppBar = styled(({ offsetY, ...rest }) => <AppBar {...rest} />)(({ offsetY, theme }) => ({
   transition: theme.transitions.create(['background-color', 'box-shadow'], {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.shortest,
@@ -45,7 +37,16 @@ export default () => {
   });
 
   return (
-    <StyledAppbar offsetY={offsetY}>
+    <StyledAppBar style={{
+      boxShadow: `0px 2px 4px -1px rgba(0,0,0,${Math.min(
+        0.2,
+        offsetY / THRESHOLD,
+      )}), 0px 4px 5px 0px rgba(0,0,0,${Math.min(
+        0.14,
+        offsetY / THRESHOLD,
+      )}), 0px 1px 10px 0px rgba(0,0,0,${Math.min(0.14, offsetY / THRESHOLD)})`,
+      backgroundColor: `rgba(255, 255, 255, ${offsetY/100})`,
+    }}>
       <StyledToolbar>
         <Wrapper>
           <Button>Home</Button>
@@ -55,6 +56,6 @@ export default () => {
           </div>
         </Wrapper>
       </StyledToolbar>
-    </StyledAppbar>
+    </StyledAppBar>
   );
 };
